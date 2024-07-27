ded6de14889629039b126ce4bb5470c29555af26
ROHITH R <rohith018.r@gmail.com>
2024-07-28 00:55:11 +0530

    Add Navbar

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,21 @@
+import { styled } from "styled-components"
+
+const Container = styled.div`
+    height:30px;
+    background-color:teal;
+    color:white;
+    display:flex;
+    justify-content:centre;
+    font-size:14px;
+    font-weight:500;
+`
+const Announcement = () => {
+    return (
+        <Container>
+            ENTER TEXT
+            Announcement.js
+        </Container>
+    );
+};
+
+export default Announcement
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,93 @@
+import { Search } from '@mui/icons-material'
+import { Badge } from '@mui/material'
+import React from 'react'
+import { styled } from 'styled-components'
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { grey } from '@mui/material/colors';
+const Container = styled.div`
+    height:60px;
+`
+const Wrapper = styled.div`
+    padding:10px 20px;
+    display:flex;
+    align-items:centre;
+    justify-content:space-between;
+`
+const Left = styled.div`
+    flex:1;
+    display:flex;
+    align-items:centre;
+    /* width:33.3%; */
+`
+const Language = styled.span`
+    font-size:14px;
+    cursor:pointer;
+`
+const SearchContainer = styled.span`
+    border:0.5px solid lightgray;
+    display:flex;
+    align-items:centre;
+    margin-left:25px;
+    padding:5px;
+    border-radius:25px;
+
+`
+const Input = styled.input`
+    border:none;
+`
+
+const Centre = styled.div`
+    /* width:33.3%; */
+    flex:1;
+    text-align:centre;
+    display:flex;
+`
+
+const Logo = styled.h1`
+    font-weight:bold;
+    
+`
+const Right = styled.div`
+    /* width:33.3%; */
+    flex:1;
+    display:flex;
+    align-items:centre;
+    justify-content:flex-end;
+
+
+`
+
+const MenuItem = styled.div`
+    font-size:14px;
+    cursor:pointer;
+    margin-left:25px;
+`
+const Navbar = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>En</Language>
+                    <SearchContainer>
+                        <Input />
+                        <Search style={{ color: "black", fontSize: 25 }} />
+                    </SearchContainer>
+                </Left>
+                <Centre>
+                    <Logo>Logo</Logo>
+                </Centre>
+                <Right>
+                    <MenuItem>Register</MenuItem>
+                    <MenuItem>Sign IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="secondary">
+                            <ShoppingCartOutlinedIcon />
+                        </Badge>
+                    </MenuItem>
+                </Right>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Navbar
